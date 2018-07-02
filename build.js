c1175c8dd1e0a8b082a99c2f68bce858960fe86c
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-01 19:59:45 -0600

    boilerplate

--- build.js
+++ build.js
@@ -8,3 +8,12 @@
  * @const {Module}
  */
 const showdown = require("showdown");
+
+/**
+ * Showdown converter.
+ * @const {Converter}
+ */
+const converter = new showdown.Converter();
+
+
+console.log(converter.makeHtml('# Title\n```JavaScript\ntest\n```'));
