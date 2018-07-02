6875bceb158c29111ae6f85926077655e335d630
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-01 21:06:58 -0600

    add test page

--- build.js
+++ build.js
@@ -3,17 +3,59 @@
  */
 "use strict";
 
+
 /**
  * Modules.
  * @const {Module}
  */
+const fs = require("fs-extra");
 const showdown = require("showdown");
-
 /**
  * Showdown converter.
  * @const {Converter}
  */
 const converter = new showdown.Converter();
 
+/**
+ * Generate content of the index file.
+ * @function
+ * @param {Object} config - The configuration object.
+ * @return {string} The generated content
+ */
+const index_content = (config) => {
+    return "TODO";
+};
 
-console.log(converter.makeHtml('# Title\n```JavaScript\ntest\n```'));
+/**
+ * Main function
+ * @async @function
+ */
+const main = async () => {
+    let config = await fs.readFile("./src/config.json", "utf8");
+    config = JSON.parse(config);
+
+    let index = await fs.readFile("./src/index.html", "utf8")
+    index = index
+        .replace("{{csp}}", config.csp)
+        .replace("{{content}}", index_content(config));
+    await fs.writeFile("./docs/index.html", index, "utf8");
+
+    let template = await fs.readFile("./src/page.html", "utf8");
+    template = template.replace("{{csp}}", config.csp);
+
+    for (const page of config.pages) {
+        let content = await fs.readFile("./src/pages/" + page.src + ".MD", "utf8");
+        content = converter.makeHtml(content);
+
+        let output = template
+            .replace("{{title}}", "Blog - " + page.title)
+            .replace("{{content}}", content);
+        await fs.writeFile("./docs/" + page.src + ".html", output, "utf8");
+    }
+};
+
+
+process.on("unhandledRejection", (e) => {
+    throw e;
+});
+main();
